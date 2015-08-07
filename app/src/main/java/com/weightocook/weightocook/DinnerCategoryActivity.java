4969f35790853a0cb4bfee737ea4291a387cbdc7
Ashley <ashleymmccarter@gmail.com>
2015-08-06 20:54:35 -0600

    DinnerCategoryActivity's search box uses existing SearchResultsActivity.java

--- app/src/main/java/com/weightocook/weightocook/DinnerCategoryActivity.java
+++ app/src/main/java/com/weightocook/weightocook/DinnerCategoryActivity.java
@@ -1,11 +1,18 @@
 package com.weightocook.weightocook;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class DinnerCategoryActivity extends AppCompatActivity {
+public class DinnerCategoryActivity extends AppCompatActivity{
+
+    public final static String SEARCH_RESULTS = "com.weightocook.weightocook.SEARCH_RESULTS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,4 +41,14 @@ public class DinnerCategoryActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /** Called when the user clicks the Search button */
+    public void recipeSearch(View view){
+        Intent intent = new Intent(this, SearchResultsActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editSearch); //gets the EditText element from the search box
+        String searchResultsStr = editText.getText().toString(); // turn the search results into a string for use in next method
+        intent.putExtra(SEARCH_RESULTS, searchResultsStr); // this is packaging the contents of editText for use in the new activity
+        startActivity(intent);
+    }
+
 }
