c7ea43ac7b9ff86fdba0494b0657bb28de73469a
Ashley <ashleymmccarter@gmail.com>
2015-09-08 16:39:36 -0600

    Added Bluetooth methods: findBluetooth() openBluetooth() beginListenForData() sendData() (commented out, for future ref) closeBluetooth()

--- app/src/main/java/com/weightocook/weightocook/HomeActivity.java
+++ app/src/main/java/com/weightocook/weightocook/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.weightocook.weightocook;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,10 +12,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
 public class HomeActivity extends AppCompatActivity {
 
     /** for recipeSearch method: "For the next activity to query the extra data, you should define the key for your intent's extra using a public constant" */
     public final static String SEARCH_RESULTS = "com.weightocook.weightocook.SEARCH_RESULTS";
+    private final static int REQUEST_ENABLE_BT = 1;
+    BluetoothDevice mmDevice;
+    BluetoothSocket mmSocket;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
 
 
     @Override
@@ -41,7 +61,7 @@ public class HomeActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    /** Called when the user clicks the Search button */
+    /** Called when the user taps the Search button */
     public void recipeSearch(View view){
         Intent intent = new Intent(this, SearchResultsActivity.class);
         EditText editText = (EditText) findViewById(R.id.editSearch); //gets the EditText element from the search box
@@ -49,12 +69,125 @@ public class HomeActivity extends AppCompatActivity {
         intent.putExtra(SEARCH_RESULTS, searchResultsStr); // this is packaging the contents of editText for use in the new activity
         startActivity(intent);
     }
+    /** Called when the user taps the Dinner button */
     public void selectDinner(View view){
         Intent intent = new Intent(this, DinnerCategoryActivity.class);
         startActivity(intent);
     }
+    /** Called when the user taps the Desserts button */
     public void selectDesserts(View view){
         Intent intent2 = new Intent(this, DessertsCategoryActivity.class);
         startActivity(intent2);
     }
+
+    /** Checks whether Bluetooth is supported and enabled - requests to enable if not */
+    public void findBluetooth() {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+        /** Pops up request to enable if found not enabled */
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        /** Get reference to scale as Bluetooth device "mmDevice" */
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                if(device.getName().equals("NameOfScalesBluetoothModule"))
+                {
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+    }
+    /** Opens connection to mmDevice */
+    void openBluetooth() throws IOException
+    {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        mmSocket.connect();
+        mmOutputStream = mmSocket.getOutputStream();
+        mmInputStream = mmSocket.getInputStream();
+
+        beginListenForData();
+    }
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        // TODO: Update readBuffer size above to match longest message size we might receive
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            // TODO: Use data string, replace: " myLabel.setText(data); "
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+/** Following for future reference only; remove later.
+    void sendData() throws IOException
+    {
+        String msg = myTextbox.getText().toString();
+        msg += "\n";
+        mmOutputStream.write(msg.getBytes());
+        myLabel.setText("Data Sent");
+    }
+*/
+    void closeBlutooth() throws IOException
+    {
+        stopWorker = true;
+        mmOutputStream.close();
+        mmInputStream.close();
+        mmSocket.close();
+    }
+
 }
